c90f8a97e6a170b891b9100a0df311412ad45f06
unknown <Ssavanu@L-OFC758BP32.americas.gap.com>
2015-06-24 11:17:44 -0400

    Added HashMap to the Functionality convTime

--- src/main/babySitter.java
+++ src/main/babySitter.java
@@ -1,5 +1,8 @@
 package main;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class babySitter {
 	
 	private static final int MIDNIGHT = 24;
@@ -7,14 +10,29 @@ public class babySitter {
 	private static final int START_TIME_TO_BED_TIME_PAY = 12;
 	private static final int BED_TIME_TO_MIDNIGHT_PAY = 8;
 	
-	private static int convertTime(int time)
+	public static int calculatePay(int startTime, int bedTime, int endTime)
 	{
-		if(time < 12)
-			time += 24;
+		int pay = 0;
 		
-		return time;
+		Map<Integer, Integer> mapA = new HashMap<Integer, Integer>();
+		
+		mapA.put(1, 25);
+		mapA.put(2, 26);
+		mapA.put(3, 27);
+		mapA.put(4, 28);
+		
+		int start = (mapA.get(startTime) == null) ? startTime : mapA.get(startTime);
+		int bed = (mapA.get(bedTime) == null) ? bedTime : mapA.get(bedTime);
+		int end = (mapA.get(endTime) == null) ? endTime : mapA.get(endTime);
+		
+		pay += calcStartToBedPay(start, bed, end);
+		pay += calcBedToMidnightPay(start, bed, end);
+		pay += calcMidnightToEndPay(start, bed, end);
+		
+		return pay;
+		
 	}
-	
+
 	private static int calcStartToBedPay(int start, int bed, int end)
 	{
 		if((start <= bed) && (start <= MIDNIGHT))
@@ -37,23 +55,6 @@ public class babySitter {
 			return (end-Math.max(start, MIDNIGHT))*AFTER_MIDNIGHT_PAY;
 
 		return 0;
-	}
-	
-	public static int calculatePay(int startTime, int bedTime, int endTime)
-	{
-		int pay = 0;
-		int start, bed, end;
-		
-		start = convertTime(startTime);
-		bed = convertTime(bedTime);
-		end = convertTime(endTime);
-		 
-		pay += calcStartToBedPay(start, bed, end);
-		pay += calcBedToMidnightPay(start, bed, end);
-		pay += calcMidnightToEndPay(start, bed, end);
-		
-		return pay;
-		
 	}
 }
 
